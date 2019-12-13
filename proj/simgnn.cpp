614353c54fea1e1b6620675af2032afa3d5a5cb5
Xinyu Ma <ma.xinyu.26a@kyoto-u.jp>
2019-12-12 20:33:00 -0800

    Better pipeline II=1

--- proj/simgnn.cpp
+++ proj/simgnn.cpp
@@ -52,34 +52,52 @@ int load_dense(
   return idx;
 }
 
-void ntn_calc(
+void ntn_first(
   const float W[nFilters3][nFilters3][nTensorNeurons],
   const float WB[nTensorNeurons][nFilters3*2],
   const float bias[nTensorNeurons],
   const float din1[nFilters3],
-  const float din2[nFilters3],
-  float dout[nTensorNeurons]
+  float score_w[nFilters3][nTensorNeurons],
+  float score[nTensorNeurons]
 ){
-  float score_w[nFilters3][nTensorNeurons], score[nTensorNeurons];
-  #pragma HLS array_partition variable=score_w complete dim=2
-  #pragma HLS array_partition variable=score complete dim=1
-
-  memset(score_w, 0, sizeof(score_w));
   for(int k = 0; k < nTensorNeurons; k ++){
     #pragma HLS unroll
     score[k] = bias[k];
   }
-  
+
   for(int j = 0; j < nFilters3; j ++){
-    #pragma HLS pipeline
     for(int k = 0; k < nTensorNeurons; k ++){
-      #pragma HLS unroll
-      score_w[j][k] += (din1[0] * W[0][j][k] + din1[1] * W[1][j][k] + din1[2] * W[2][j][k] + din1[3] * W[3][j][k] +
-                        din1[4] * W[4][j][k] + din1[5] * W[5][j][k] + din1[6] * W[6][j][k] + din1[7] * W[7][j][k] +
-                        din1[8] * W[8][j][k] + din1[9] * W[9][j][k] + din1[10] * W[10][j][k] + din1[11] * W[11][j][k] +
-                        din1[12] * W[12][j][k] + din1[13] * W[13][j][k] + din1[14] * W[14][j][k] + din1[15] * W[15][j][k]);
+      #pragma HLS pipeline
+      score_w[j][k] = din1[0] * W[0][j][k] + din1[1] * W[1][j][k] + din1[2] * W[2][j][k] + din1[3] * W[3][j][k] +
+                      din1[4] * W[4][j][k] + din1[5] * W[5][j][k] + din1[6] * W[6][j][k] + din1[7] * W[7][j][k] +
+                      din1[8] * W[8][j][k] + din1[9] * W[9][j][k] + din1[10] * W[10][j][k] + din1[11] * W[11][j][k] +
+                      din1[12] * W[12][j][k] + din1[13] * W[13][j][k] + din1[14] * W[14][j][k] + din1[15] * W[15][j][k];
     }
   }
+  for(int k = 0; k < nTensorNeurons; k ++){
+    #pragma HLS unroll
+    for(int i = 0; i < nFilters3; i ++){
+      #pragma HLS unroll
+      score[k] += WB[k][i] * din1[i];
+    }
+  }
+}
+
+void ntn_next(
+  const float WB[nTensorNeurons][nFilters3*2],
+  const float din2[nFilters3],
+  const float score_w[nFilters3][nTensorNeurons],
+  const float score_0[nTensorNeurons],
+  float dout[nTensorNeurons]
+){
+  float score[nTensorNeurons];
+  #pragma HLS array_partition variable=score complete dim=1
+
+  for(int k = 0; k < nTensorNeurons; k ++){
+    #pragma HLS unroll
+    score[k] = score_0[k];
+  }
+
   for(int j = 0; j < nFilters3; j ++){
     #pragma HLS unroll
     for(int k = 0; k < nTensorNeurons; k ++){
@@ -90,10 +108,6 @@ void ntn_calc(
 
   for(int k = 0; k < nTensorNeurons; k ++){
     #pragma HLS unroll
-    for(int i = 0; i < nFilters3; i ++){
-      #pragma HLS unroll
-      score[k] += WB[k][i] * din1[i];
-    }
     for(int i = 0; i < nFilters3; i ++){
       #pragma HLS unroll
       score[k] += WB[k][nFilters3 + i] * din2[i];
@@ -166,22 +180,20 @@ void l3_calc(
 }
 
 float process_pair(
-  const float NTN_W[nFilters3][nFilters3][nTensorNeurons],
   const float NTN_WB[nTensorNeurons][nFilters3*2],
-  const float NTN_bias[nTensorNeurons],
+  const float score_w[nFilters3][nTensorNeurons],
+  const float score_0[nTensorNeurons],
   const float L1_W[nDense1][nScores],
   const float L1_bias[nDense1],
   const float L2_W[nDense2][nDense1],
   const float L2_bias[nDense2],
   const float L3_W[nDense3][nDense2],
   const float L3_bias[nDense3],
-  const float row_emb[SIZE_EMBEDDING],
   const float col_emb[SIZE_EMBEDDING]
 ){
-  float l[SIZE_EMBEDDING], r[SIZE_EMBEDDING];
+  float r[SIZE_EMBEDDING];
   float ntn_ret[nTensorNeurons];
   float l1_ret[nDense1], l2_ret[nDense2], l3_ret[nDense3];
-  #pragma HLS array_partition variable=l complete dim=1
   #pragma HLS array_partition variable=r complete dim=1
   #pragma HLS array_partition variable=ntn_ret complete dim=1
   #pragma HLS array_partition variable=l1_ret complete dim=1
@@ -190,14 +202,10 @@ float process_pair(
 
   for(int i = 0; i < SIZE_EMBEDDING; i ++){
     #pragma HLS unroll
-    l[i] = row_emb[i];
-  }
-  for(int i = 0; i < SIZE_EMBEDDING; i ++){
-    #pragma HLS unroll
     r[i] = col_emb[i];
   }
 
-  ntn_calc(NTN_W, NTN_WB, NTN_bias, l, r, ntn_ret);
+  ntn_next(NTN_WB, r, score_w, score_0, ntn_ret);
   l1_calc(L1_W, L1_bias, ntn_ret, l1_ret);
   l2_calc(L2_W, L2_bias, l1_ret, l2_ret);
   l3_calc(L3_W, L3_bias, l2_ret, l3_ret);
@@ -233,8 +241,7 @@ void simgnn_kernel(
   float NTN_bias[nTensorNeurons];
 
   #pragma HLS array_partition variable=NTN_W complete dim=1
-  #pragma HLS array_partition variable=NTN_W cyclic dim=2 factor=2
-  #pragma HLS array_partition variable=NTN_W complete dim=3
+  #pragma HLS array_partition variable=NTN_W cyclic dim=3 factor=2
   #pragma HLS array_partition variable=NTN_WB complete dim=1
   #pragma HLS array_partition variable=NTN_WB complete dim=2
   #pragma HLS array_partition variable=NTN_bias complete dim=1
@@ -264,6 +271,12 @@ void simgnn_kernel(
   #pragma HLS array_partition variable=ce cyclic dim=1 factor=4
   #pragma HLS array_partition variable=result_cache cyclic dim=1 factor=4
 
+  float score_w[nFilters3][nTensorNeurons];
+  float score_0[nTensorNeurons];
+  #pragma HLS array_partition variable=score_w complete dim=1
+  #pragma HLS array_partition variable=score_w complete dim=2
+  #pragma HLS array_partition variable=score_0 complete dim=1
+
   assert(nrow == NROW);
   assert(ncol == NCOL);
 
@@ -288,10 +301,12 @@ void simgnn_kernel(
 
   for(int row = 0; row < nrow; row ++){
     #pragma HLS LOOP_TRIPCOUNT max=1200 min=1200
+
+    ntn_first(NTN_W, NTN_WB, NTN_bias, re[row], score_w, score_0);
     for(int col = 0; col < ncol; col ++){
       #pragma HLS pipeline
       #pragma HLS LOOP_TRIPCOUNT max=1200 min=1200
-      result_cache[col] = process_pair(NTN_W, NTN_WB, NTN_bias, L1_W, L1_bias, L2_W, L2_bias, L3_W, L3_bias, re[row], ce[col]);
+      result_cache[col] = process_pair(NTN_WB, score_w, score_0, L1_W, L1_bias, L2_W, L2_bias, L3_W, L3_bias, ce[col]);
     }
 
     for(int col = 0; col < ncol; col ++){
